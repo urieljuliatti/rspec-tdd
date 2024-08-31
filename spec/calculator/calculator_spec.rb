557bc36e956a5b916ba5d7175cedae1caaa6b5b2
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-08-31 08:55:22 -0300

    Matchers com erros

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -22,9 +22,12 @@ describe Calculator do
 
   # Subject implícito
   context '#div' do
-    it 'divide by zero' do
+    it 'divided by zero' do
       #expect{subject.div(3, 0)}.to raise_exception # Colocando em um bloco pra avaliar o erro e pegar a exceção, mas é muito genérico
       expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError) # Colocando em um bloco pra avaliar o erro e pegar a exceção. Essa é a forma mais apropriada
+      expect{subject.div(3, 0)}.to raise_error('divided by 0') # Colocando em um bloco pra avaliar o erro e pegar a exceção. Essa é a forma mais usando o texto
+      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError, 'divided by 0') # Com  o texto e a exceção
+      expect{subject.div(3, 0)}.to raise_error(/divided/) # Com expressão regular
     end
   end
 
