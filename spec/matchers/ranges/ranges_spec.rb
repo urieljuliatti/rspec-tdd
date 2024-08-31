fd2b27e5d470cfb68396c4c33a7f14de170ab53f
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-08-31 09:32:31 -0300

    is_expected one-liner syntax

--- spec/matchers/ranges/ranges_spec.rb
+++ spec/matchers/ranges/ranges_spec.rb
@@ -3,4 +3,7 @@ describe (1..5), 'Matchers para Ranges' do
     expect(subject).to cover(2)
     expect(subject).to cover(2, 5)
   end
+
+  # is_expected já infere o subject e é boa para one-liner
+  it { is_expected.not_to cover(0, 6) }
 end
