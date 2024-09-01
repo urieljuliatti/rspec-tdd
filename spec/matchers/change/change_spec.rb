0ab85f3ab8d38d5a9f738ed235a43a10b4bacaad
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-09-01 04:08:44 -0300

    Matcher change

---
+++ spec/matchers/change/change_spec.rb
@@ -0,0 +1,7 @@
+require 'contador'
+
+RSpec.describe 'Matchers de Change' do
+  it { expect{Contador.incrementa}.to change { Contador.qtd } } #qtd 1
+  it { expect{Contador.incrementa}.to change { Contador.qtd }.by(1) } #qtd 2
+  it { expect{Contador.incrementa}.to change { Contador.qtd }.from(2).to(3) } #qtd 3
+end
