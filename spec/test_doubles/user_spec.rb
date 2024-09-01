022928f6d7df990d3db61b6bbdd5d9527e2bc10d
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-09-01 05:47:50 -0300

    Test Doubles

---
+++ spec/test_doubles/user_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe 'Test Double' do
+  it '--' do
+    user = double('User')
+    allow(user).to receive(:name).and_return('Uriel')
+    allow(user).to receive(:password).and_return('secret')
+    puts user.name
+    puts user.password
+  end
+end
