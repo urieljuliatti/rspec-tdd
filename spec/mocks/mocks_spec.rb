af29f992c24b85220da0f3fdb20f4d5ad176ca8a
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-09-01 06:51:09 -0300

    Mock Expectations

--- spec/mocks/mocks_spec.rb
+++ spec/mocks/mocks_spec.rb
@@ -10,4 +10,23 @@ RSpec.describe 'Mocks' do
     # exercise
     student.bar
   end
+
+  it 'args' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123)
+    student.foo(123)
+  end
+
+  it 'repetição' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123).twice
+    student.foo(123)
+    student.foo(123)
+  end
+
+  it 'com retorno' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123).and_return(true)
+    puts student.foo(123)
+  end
 end
