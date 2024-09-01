33431c4f93254b977c96edc29ec1d632b91f1976
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-09-01 04:29:02 -0300

    Negativando Matchers

--- spec/matchers/arrays/arrays_spec.rb
+++ spec/matchers/arrays/arrays_spec.rb
@@ -1,9 +1,13 @@
+RSpec::Matchers.define_negated_matcher :exclude, :include
+
 describe Array.new([1,2,3]), 'Matcher de Arrays' do
   # passa elementos
   it '#include' do
     expect(subject).to include(2)
     expect(subject).to include(2,1)
   end
+
+  it { expect(subject).to exclude(4) }
 
   # elementos exatos com a array
   it '#match_array' do
--- spec/matchers/be_within/be_within_spec.rb
+++ spec/matchers/be_within/be_within_spec.rb
@@ -1,5 +1,8 @@
+RSpec::Matchers.define_negated_matcher :be_not_within, :be_within
+
 RSpec.describe 'Matchers be_within' do
   # é um intervalo de um delta de 0.5
   it{ expect(12.5).to be_within(0.5).of(12.0) }
+  it{ expect(11.4).to be_not_within(0.5).of(12.0) }
   it{ expect([11.6, 12.1, 12.4]).to all (be_within(0.5).of(12.0))}
 end
