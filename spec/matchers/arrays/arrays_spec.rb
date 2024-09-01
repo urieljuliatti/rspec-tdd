bdb529e1e031716ca9f279fa14247949011bb0da
Uriel Juliatti <uriel.juliattivalle@gmail.com>
2024-09-01 05:22:45 -0300

    Adicionando um tag filter collection. Para rodar: rspec . -t type:collection

--- spec/matchers/arrays/arrays_spec.rb
+++ spec/matchers/arrays/arrays_spec.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define_negated_matcher :exclude, :include
 
-describe Array.new([1,2,3]), 'Matcher de Arrays', collection: true do
+describe Array.new([1,2,3]), 'Matcher de Arrays', type: 'collection' do
   # passa elementos
   it '#include' do
     expect(subject).to include(2)
